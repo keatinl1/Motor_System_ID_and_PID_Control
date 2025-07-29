dd3cb54cd48c968d4f664ebc1aac54a894155467
Luke <keatinl1@tcd.ie>
2025-07-29 16:14:10 +0100

    double checked, happy w edge case handling

--- dc_motor_id_and_control.ino
+++ dc_motor_id_and_control.ino
@@ -9,7 +9,6 @@
 #define MOTOR_A      20   // motor pin (A0)
 #define MOTOR_B      21   // motor pin (A1)
 #define MOTOR_ENABLE 29   // pwm pin   (B1)
-#define DEG_TO_RAD 0.017453292519943295
 
 // instantiate objects
 L293D motor(MOTOR_A, MOTOR_B, MOTOR_ENABLE);
@@ -23,28 +22,28 @@ void setup() {
   Wire.begin();
 
   // Timer in "timed out" state
-  silent_timer.setTimeOutTime(1);
-  silent_timer.reset();
-  delay(2);
-  silent_timer.setTimeOutTime(4); // 250Hz
+  silent_timer.setTimeOutTime(4); // 4ms timeout = 250Hz
   silent_timer.reset();
 }
 
 void loop() {
   if (silent_timer.hasTimedOut()) {
-    // angular position
+    // get angular position
     static float prev_angle = 0.0;
     float curr_angle = encoder.angleRead();
-    
-    // change in angular position
-    float delta_angle = curr_angle - prev_angle;
-    if (delta_angle > 180.0) delta_angle -= 360.0;
-    if (delta_angle < -180.0) delta_angle += 360.0;
 
-    float omega = delta_angle * 4.36332313; // rad/
+    // change in angular position with edge case
+    if (prev_angle > 180.0 && curr_angle < 180.0) {
+      prev_angle = prev_angle - 360;
+    }
+    float delta_angle = curr_angle - prev_angle;
+    
+    // change in angle to rad divide by change in const timestep
+    float omega = delta_angle * 4.36332313;
 
     Serial.print("omega: ");
-    Serial.println(omega);
+    Serial.print(omega);
+    Serial.println("rad/s");    
 
     motor.SetMotorSpeed(3.0); // voltage input
 
